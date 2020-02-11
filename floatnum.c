83862cad2c15284a1373f1f7bc1e9848a6f46c9f
RameshSangameswaran <33337734+RameshSangameswaran@users.noreply.github.com>
2020-02-11 10:29:31 +0530

    Add files via upload
    To get the product of two floating numbers

---
+++ floatnum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+float a,b,product;
+printf("Input first number");
+scanf("%f", &a);
+printf("Input second number");
+scanf("%f", &b);
+product=a * b;
+printf("The multiplication of two float number %.2f & %.2f is %.2f\n",a,b,product);
+}
+
